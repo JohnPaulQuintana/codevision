aefc4d1ba6cd8e3a4d6e80a1747c1bcb744dd14e
ExousiaNavi <jpquintana2024@gmail.com>
2026-04-26 00:54:56 +0800

    Update QuizSimulation.tsx

--- src/pages/modules/oop/Lesson1/quiz/QuizSimulation.tsx
+++ src/pages/modules/oop/Lesson1/quiz/QuizSimulation.tsx
@@ -563,7 +563,7 @@ pokemon.display();
         </div>
 
         {/* RIGHT - CODE EDITOR */}
-        <div className="w-full md:w-[55%] bg-[#0F172A] border-l border-gray-800 flex flex-col h-screen">
+        <div className="w-full md:w-[55%] bg-[#0F172A] border-l border-gray-800 flex flex-col">
           <div className="flex-1 px-3">
             <QuizEditor
               code={code}
